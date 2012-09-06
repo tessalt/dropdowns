21c786800d578a05dcdac3953bf20e4fc63cb9ee
Tessa Thornton <tessathornton@gmail.com>
2012-09-05 20:12:45 -0400

    finally got browser resizing behavioru to work properly

--- script.js
+++ script.js
@@ -1,43 +1,50 @@
-var adjustMenu = function() {
 
-	$(".nav li  a").each(function() {
+var ww = document.body.clientWidth;
+
+$(document).ready(function() {
+	$(".nav li a").each(function() {
 		if ($(this).next().length > 0) {
 			$(this).addClass("parent");
 		};
 	})
 	
 	$(".toggleMenu").click(function(e) {
-		// can't use toggle because we don't know original state
 		e.preventDefault();
-		if ($(".nav").css("display") === "none") {
-			$(".nav").show();
-		} else {
-	 		$(".nav").hide();
-	 	}
+		$(this).toggleClass("active");
+		$(".nav").toggle();
 	});
-	
-	if ($(window).width() < 768) {
+	adjustMenu();
+})
+
+$(window).bind('resize orientationchange', function() {
+	ww = document.body.clientWidth;
+	adjustMenu();
+});
+
+var adjustMenu = function() {
+	if (ww < 768) {
 		$(".toggleMenu").css("display", "inline-block");
-		$(".nav").hide();
+		if (!$(".toggleMenu").hasClass("active")) {
+			$(".nav").hide();
+		} else {
+			$(".nav").show();
+		}
 		$(".nav li").unbind('mouseenter mouseleave');
-		$(".nav li a").unbind('click').bind('click', function() {
+		$(".nav li a.parent").unbind('click').bind('click', function(e) {
 			// must be attached to anchor element to prevent bubbling
+			e.preventDefault();
 			$(this).parent("li").toggleClass("hover");
 		});
 	} 
-
-	else if ($(window).width() >= 768){
+	else if (ww >= 768) {
 		$(".toggleMenu").css("display", "none");
 		$(".nav").show();
+		$(".nav li").removeClass("hover");
 		$(".nav li a").unbind('click');
 		$(".nav li").unbind('mouseenter mouseleave').bind('mouseenter mouseleave', function() {
 		 	// must be attached to li so that mouseleave is not triggered when hover over submenu
 		 	$(this).toggleClass('hover');
 		});
 	}
-};
+}
 
-
-$(window).bind('load resize orientationchange', function() {
-	adjustMenu();
-});
